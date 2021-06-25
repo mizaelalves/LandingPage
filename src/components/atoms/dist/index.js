45b0a2525f4ffd1137f4cc125ae2e8630a20dd81
Mizael Alves <mizaelbna@hotmail.com>
2021-06-25 01:50:39 -0300

    landing page portifolio

---
+++ src/components/atoms/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+var Logo_vue_1 = require("./Logo.vue");
+__createBinding(exports, Logo_vue_1, "default", "Logo");
+var NavItem_vue_1 = require("./NavItem.vue");
+__createBinding(exports, NavItem_vue_1, "default", "NavItem");
+var SocialLink_vue_1 = require("./SocialLink.vue");
+__createBinding(exports, SocialLink_vue_1, "default", "SocialLink");
+var Credits_vue_1 = require("./Credits.vue");
+__createBinding(exports, Credits_vue_1, "default", "Credits");
